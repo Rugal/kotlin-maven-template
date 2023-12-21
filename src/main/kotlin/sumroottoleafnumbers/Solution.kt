304704bd979b33e90cdc18d8ae644677f77b53a6
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2023-12-21 23:55:46 +0800

    Add Sum Root to Leaf Numbers

---
+++ src/main/kotlin/sumroottoleafnumbers/Solution.kt
@@ -0,0 +1,29 @@
+package sumroottoleafnumbers
+
+import countgoodnodesinbinarytree.TreeNode
+
+class Solution {
+
+  private var result = 0
+
+  private fun dfs(root: TreeNode?, current: Int) {
+    if (null == root) {
+      return
+    }
+    val next = current * 10 + root.`val`
+    // if leaf
+    if (root.left == null && root.right == null) {
+      result += next
+      return
+    }
+
+    this.dfs(root.left, next)
+    this.dfs(root.right, next)
+  }
+
+  fun sumNumbers(root: TreeNode?): Int {
+    this.result = 0
+    this.dfs(root, 0)
+    return this.result
+  }
+}
