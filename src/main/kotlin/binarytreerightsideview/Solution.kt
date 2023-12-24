d2414cbcc2a492f1828923dfbf90197bfc6dcb2c
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2023-12-24 18:43:20 +0800

    Add 199. Binary Tree Right Side View

---
+++ src/main/kotlin/binarytreerightsideview/Solution.kt
@@ -0,0 +1,30 @@
+package binarytreerightsideview
+
+import countgoodnodesinbinarytree.TreeNode
+
+class Solution {
+  private val result = mutableListOf<Int>()
+  private var maxLevel = -1
+
+  private fun dfs(root: TreeNode?, level: Int) {
+    if (null == root) return
+
+    // the very first visit of a new level must be the rightmost node to be seen from right side view
+    if (this.maxLevel < level) {
+      this.maxLevel = level
+      this.result.add(root.`val`)
+    }
+    // traverse right first
+    this.dfs(root.right, level + 1)
+    this.dfs(root.left, level + 1)
+  }
+
+  fun rightSideView(root: TreeNode?): List<Int> {
+    this.result.clear()
+    if (null == root) return this.result
+
+    this.dfs(root, 0)
+
+    return this.result
+  }
+}
