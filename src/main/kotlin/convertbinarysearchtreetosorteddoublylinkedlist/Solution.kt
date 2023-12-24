92deb7f765ccc2672c2d18fd1200ec3d671a8a41
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2023-12-24 23:53:29 +0800

    Add 426. Convert Binary Search Tree to Sorted Doubly Linked List

---
+++ src/main/kotlin/convertbinarysearchtreetosorteddoublylinkedlist/Solution.kt
@@ -0,0 +1,44 @@
+package convertbinarysearchtreetosorteddoublylinkedlist
+
+import java.util.Stack
+
+class Solution {
+  private val s = Stack<Node>()
+
+  private fun dfs(root: Node?) {
+    if (null == root) return
+
+    this.dfs(root.right)
+    this.s.push(root)
+    this.dfs(root.left)
+  }
+
+  fun treeToDoublyList(root: Node?): Node? {
+    if (null == root) return null
+
+    this.dfs(root)
+
+    val root = this.s.pop()
+
+    var current = root
+    while (s.isNotEmpty()) {
+      val pop = this.s.pop()
+
+      current.right = pop
+      pop.left = current
+
+      current = current.right
+    }
+
+    current.right = root
+    root.left = current
+
+    return root
+  }
+}
+
+data class Node(
+  var `val`: Int,
+  var left: Node? = null,
+  var right: Node? = null,
+) 
