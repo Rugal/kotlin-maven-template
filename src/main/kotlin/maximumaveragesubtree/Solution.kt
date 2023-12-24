b755d1469f1bd8d0176d676a77e21fe1eb88cfa4
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2023-12-24 22:06:51 +0800

    Add 1120. Maximum Average Subtree

---
+++ src/main/kotlin/maximumaveragesubtree/Solution.kt
@@ -0,0 +1,38 @@
+package maximumaveragesubtree
+
+import kotlin.math.max
+import countgoodnodesinbinarytree.TreeNode
+
+/**
+ * https://leetcode.com/problems/maximum-average-subtree/description/
+ */
+class Solution {
+
+  private var result = 0.0
+
+  /**
+   * @return pair of sum and node count
+   */
+  private fun dfs(root: TreeNode?): Pair<Int, Int> {
+    if (null == root) return 0 to 0
+
+    val left = this.dfs(root.left)
+    val right = this.dfs(root.right)
+
+    val sum = left.first + right.first + root.`val`
+    val count = left.second + right.second + 1
+
+    this.result = max(this.result, 1.0 * sum / count)
+
+    return sum to count
+  }
+
+  fun maximumAverageSubtree(root: TreeNode?): Double {
+    this.result = 0.0
+    if (null == root) return result
+
+    this.dfs(root)
+
+    return this.result
+  }
+}
