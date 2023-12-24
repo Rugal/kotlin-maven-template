ddd1bf2ad0770fdf3fda9ecb347a8c3651dd1983
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2023-12-24 23:12:50 +0800

    Add 1530. Number of Good Leaf Nodes Pairs

---
+++ src/main/kotlin/numberofgoodleafnodespairs/Solution.kt
@@ -0,0 +1,65 @@
+package numberofgoodleafnodespairs
+
+import countgoodnodesinbinarytree.TreeNode
+
+/**
+ * https://leetcode.com/problems/number-of-good-leaf-nodes-pairs
+ */
+class Solution {
+
+  private var distance = 0
+  private var result = 0
+
+  private fun TreeNode.isLeaf(): Boolean = this.left == null && this.right == null
+
+  /**
+   * @return an array indicates how many leaf in different distance, because we only care about <= distance leaf, create an array for that
+   *
+   *  0 1 2 3 4
+   * [0 2 0 1 0] means there is 2 leaf that is 1 distance away and 1 leaf that is 3 distance away
+   * in total we only care about distance <= 4, so create array with size = distance + 1 should be enough
+   *
+   * For each subtree, we shall get both tree from both left and right subtree, before calculating array of current node
+   */
+  private fun dfs(root: TreeNode?): IntArray {
+    val r = IntArray(this.distance + 1)
+    if (null == root) return r
+
+    // leaf node
+    if (root.isLeaf()) {
+      r[1] = 1
+      return r
+    }
+
+    val left = this.dfs(root.left)
+    val right = this.dfs(root.right)
+
+    for (i in 1 until distance) {
+      for (j in 1 until distance) {
+        if (i + j <= distance) {
+          // calculation combination
+          this.result += left[i] * right[j]
+        }
+      }
+    }
+
+    // prepare for parent level
+    for (i in 1..distance) {
+      // add 1 more level
+      r[i] = left[i - 1] + right[i - 1]
+    }
+
+    return r
+  }
+
+  fun countPairs(root: TreeNode?, distance: Int): Int {
+    this.result = 0
+    this.distance = distance
+
+    if (null == root) return this.result
+
+    this.dfs(root)
+
+    return this.result
+  }
+}
