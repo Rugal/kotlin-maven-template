21831a557b3f288857685be2a7ab426f07ebb7ee
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2023-12-24 22:18:02 +0800

    Add 101. Symmetric Tree

---
+++ src/main/kotlin/symmetrictree/Solution.kt
@@ -0,0 +1,24 @@
+package symmetrictree
+
+import countgoodnodesinbinarytree.TreeNode
+
+/**
+ * https://leetcode.com/problems/symmetric-tree/
+ */
+class Solution {
+
+  private fun dfs(left: TreeNode?, right: TreeNode?): Boolean {
+    if (null == left && null == right) return true
+    if (null == left || null == right) return false
+
+    return left.`val` == right.`val`
+      && this.dfs(left.left, right.right)
+      && this.dfs(left.right, right.left)
+  }
+
+  fun isSymmetric(root: TreeNode?): Boolean {
+    if (null == root) return true
+
+    return this.dfs(root.left, root.right)
+  }
+}
