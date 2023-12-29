4d4627b706fe44c8854c739c9edb371ed7666be9
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2023-12-30 02:32:12 +0800

    Add 938. Range Sum of BST

---
+++ src/main/kotlin/rangesumofbst/Solution.kt
@@ -0,0 +1,15 @@
+package rangesumofbst
+
+import countgoodnodesinbinarytree.TreeNode
+
+class Solution {
+  fun rangeSumBST(root: TreeNode?, low: Int, high: Int): Int {
+    if (null == root) return 0
+
+    return (if (root.`val` in low..high) root.`val` else 0) + this.rangeSumBST(root.left, low, high) + this.rangeSumBST(
+      root.right,
+      low,
+      high
+    )
+  }
+}
