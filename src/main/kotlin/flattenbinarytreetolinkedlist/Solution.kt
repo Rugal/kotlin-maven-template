64873d777ff028a1fd20093256651d74d9179540
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2023-12-31 22:27:46 +0800

    Add 114. Flatten Binary Tree to Linked List

---
+++ src/main/kotlin/flattenbinarytreetolinkedlist/Solution.kt
@@ -0,0 +1,31 @@
+package flattenbinarytreetolinkedlist
+
+import java.util.LinkedList
+import countgoodnodesinbinarytree.TreeNode
+
+/**
+ * https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+ */
+class Solution {
+  private val queue = LinkedList<TreeNode>()
+
+  private fun dfs(root: TreeNode?) {
+    if (null == root) return
+
+    queue.offer(root)
+    this.dfs(root.left)
+    this.dfs(root.right)
+  }
+
+  fun flatten(root: TreeNode?) {
+    if (null == root) return
+    this.dfs(root)
+    var current: TreeNode = root
+    this.queue.pop()
+    while (this.queue.isNotEmpty()) {
+      current.left = null
+      current.right = this.queue.pop()
+      current = current.right!!
+    }
+  }
+}
