2fb9ab082b3ded6d7fda1a78857e22edb1c3064e
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2023-12-31 22:59:10 +0800

    Add 94. Binary Tree Inorder Traversal

---
+++ src/main/kotlin/binarytreeinordertraversal/Solution.kt
@@ -0,0 +1,33 @@
+package binarytreeinordertraversal
+
+import java.util.Stack
+import countgoodnodesinbinarytree.TreeNode
+
+/**
+ * https://leetcode.com/problems/binary-tree-inorder-traversal/
+ */
+class Solution {
+  fun inorderTraversal(root: TreeNode?): List<Int> {
+    val result = mutableListOf<Int>()
+
+    if (null == root) return result
+
+    val s = Stack<TreeNode>()
+    var root = root
+
+    while (null != root || s.isNotEmpty()) {
+      while (null != root) {
+        s.push(root)
+        root = root.left
+      }
+
+      if (s.isNotEmpty()) {
+        val node = s.pop()
+        result.add(node.`val`)
+        root = node.right
+      }
+    }
+
+    return result
+  }
+}
