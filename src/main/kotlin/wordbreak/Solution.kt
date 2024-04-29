b61bf45fae5bb8ca41a99cbe3e2ca88b9e092406
Rugal Bernstein <rugal.bernstein.0@gmail.com>
2024-04-29 20:42:54 +0800

    Update for 139. Word Break

--- src/main/kotlin/wordbreak/Solution.kt
+++ src/main/kotlin/wordbreak/Solution.kt
@@ -1,26 +1,26 @@
 package wordbreak
 
 /**
- * https://leetcode.com/problems/word-break/solution/
+ * https://leetcode.com/problems/word-break/
  *
  * @author Rugal Bernstein
  */
 class Solution {
-  fun wordBreak(s: String, wordDict: List<String>): Boolean {
 
+  fun wordBreak1(s: String, wordDict: List<String>): Boolean {
     val set = wordDict.toSet()
-    val result = BooleanArray(s.length + 1)
-    result[0] = true
+    val result = BooleanArray(s.length + 1) { it == 0 }
 
-    for (current in 1 until result.size) {
+    for (current in 1..s.length) { // substring excludes right
       for (i in 0 until current) {
+        // search all prefix to see if any string can match 0~i and i~current
         if (result[i] && set.contains(s.substring(i, current))) {
           result[current] = true
-          // if at least one matched, means it is solvable
-          break
+          break // if at least one matched, means it is solvable
         }
       }
     }
+
     return result[s.length]
   }
 }
